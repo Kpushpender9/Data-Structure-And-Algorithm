f1c3e4e7ca91ce7ef6f45903b2e2712f282e9f6a
Jyoti Kumari <jyoti.k@luckpay.in>
2026-03-01 21:45:54 +0530

    Initial commit

---
+++ DsaPreparation/src/main/java/recursion/NCR.java
@@ -0,0 +1,13 @@
+package recursion;
+
+public class NCR {
+	public static void main (String args[]) {
+		System.out.println(nCr(5,3));
+	}
+	
+	public static int nCr(int n,int r) {
+		if(n==r||r==0)
+			return 1;
+		return nCr(n-1, r-1)+nCr(n-1, r);
+	}
+}
